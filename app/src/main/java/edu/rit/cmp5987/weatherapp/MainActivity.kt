b924361cc2f9cfb63a4dfc68b9866f55b39736e8
Catie Poggioli <cmp5987@rit.edu>
2020-11-07 17:59:36 -0500

    Setup Permissions to Use location and print out lat and long

--- app/src/main/java/edu/rit/cmp5987/weatherapp/MainActivity.kt
+++ app/src/main/java/edu/rit/cmp5987/weatherapp/MainActivity.kt
@@ -1,26 +1,129 @@
 package edu.rit.cmp5987.weatherapp
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.location.*
 import com.google.gson.Gson
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import org.json.JSONObject
 import java.io.*
 import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
 import java.net.URL
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mFusedLocationClient : FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CallAPILoginAsyncTask("Catherine", "12345").execute()
+        //CallAPILoginAsyncTask("Catherine", "12345").execute()
+
+        //initialize
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if(!isLocationEnabled()){
+            Toast.makeText(this, "Your location provider is turned off. Please turn it on.", Toast.LENGTH_SHORT).show()
+
+            //Lead the user to where they can turn location on
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }else{
+            //Check for permissions
+            Dexter.withActivity(this).withPermissions(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+                .withListener(object : MultiplePermissionsListener{
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()){
+                            requestLocationData()
+                        }
+                        if(report.isAnyPermissionPermanentlyDenied){
+                            Toast.makeText(this@MainActivity, "You have denied location permission. Please allow location.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread().check()
+        }
+
+    }
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off permissions for this feature. It can be enabled under Application Settings.")
+            .setPositiveButton("Go TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    //link to application so the right settings are open
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    //start activity
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel"){
+                dialog, _ -> dialog.dismiss()
+            }.show()
     }
 
+    /* Get the location of the user */
+    private fun isLocationEnabled(): Boolean {
+
+        //this provides acess to the ystem location services
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData(){
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback, Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("Current latitude", "$latitude")
+            val longitude = mLastLocation.longitude
+            Log.i("Current longitude", "$longitude")
+
+        }
+    }
+
+    /*
     private inner class CallAPILoginAsyncTask(val username: String, val password: String): AsyncTask<Any, Void, String>(){
 
         private lateinit var customProgressDialog: Dialog
@@ -162,5 +265,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+     */
 
 }
